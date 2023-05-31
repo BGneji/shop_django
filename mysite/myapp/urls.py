c6a841df1d9389d99d43d4d0ce82df78cd104479
NEJI <mrkochetov@yandex.ru>
2023-05-31 11:22:12 +0300

    base template + navbar

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # http://127.0.0.1:8000/myapp/hello/
     path('', views.index),
     path('<int:my_id>/', views.indexItem, name='detail'),
-    # http://127.0.0.1:8000/myapp/contacts/
+
 ]
