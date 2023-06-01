4cd25bb3554bee26ec27a752e3a8b56aed92325e
NEJI <mrkochetov@yandex.ru>
2023-06-01 13:28:37 +0300

    register

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,6 +25,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("myapp/", include("myapp.urls", namespace="myapp")),
+    path("users/", include("users.urls", namespace="users")),
+
 
 ]
 
