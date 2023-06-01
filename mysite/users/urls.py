db7b156e3646443335695e9c675ca7ed3d99c3e8
NEJI <mrkochetov@yandex.ru>
2023-06-01 14:28:36 +0300

    login and logout

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import register
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = "user"
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
